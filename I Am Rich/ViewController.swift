78e7fd6c5e9ba8c60cc2dc2c7fd01ffc127d28c1
Chris Mathews <chrismathews@mac.com>
2020-03-19 20:01:06 +0000

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Chris Mathews on 19/03/2020.
+//  Copyright © 2020 Chris Mathews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
