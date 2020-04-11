af1dc731443d87e42b4fff0d91b006d4206079ab
Chris Mathews <chrismathews@mac.com>
2020-04-11 09:51:32 +0100

    Start of I Am Poor project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Chris Mathews on 11/04/2020.
+//  Copyright © 2020 Chris Mathews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
